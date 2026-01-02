dcc6b3a506df9bae4f3caede7dde41e6a680dcd2
ARJUN <pachangedinanath@gmail.com>
2026-01-02 19:14:07 +0530

    Update clone.py

--- Clonify/cplugin/clone.py
+++ Clonify/cplugin/clone.py
@@ -12,7 +12,7 @@ from .utils import StartTime
 from Clonify.utils import get_readable_time
 from Clonify.utils.decorators.language import language
 
-APP_LINK = f"https://t.me/CaptainAmericaXVBot"
+APP_LINK = f"https://t.me/dark_powerfull_bot"
 
 
 @Client.on_message(filters.command("clone"))
