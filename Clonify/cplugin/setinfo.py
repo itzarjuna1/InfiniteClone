bb0c27f0e13c9bca59ebcb1576650e5701d25888
ARJUN <pachangedinanath@gmail.com>
2026-01-02 19:15:16 +0530

    Update setinfo.py

--- Clonify/cplugin/setinfo.py
+++ Clonify/cplugin/setinfo.py
@@ -129,8 +129,8 @@ async def bot_info(client: Client, message: Message, _):
     await message.reply_text(
         f"**Bᴏᴛ Iɴғᴏ:**\n"
         f"➤ **Bᴏᴛ ID:** `{bot_id}`\n"
-        f"➤ **Cʜᴀɴɴᴇʟ:** @DRxGiveway\n"
-        f"➤ **Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ:** @DRx_SupportChat\n"
+        f"➤ **Cʜᴀɴɴᴇʟ:** @dark_musictm\n"
+        f"➤ **Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ:** @snowy_hometown\n"
         f"➤ **Bᴏᴛ Sᴛᴀᴛᴜs:** {bot_status}"
     )
 
