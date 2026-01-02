83a9edaf9e3471d48f7901e7aa724150d9b8cfe6
ARJUN <pachangedinanath@gmail.com>
2026-01-02 19:11:36 +0530

    Update start.py

--- Clonify/utils/inline/start.py
+++ Clonify/utils/inline/start.py
@@ -33,8 +33,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=[𝗗𝗘𝗩], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=[𝗖𝗢-𝗗𝗘𝗩], url=f"https://t.me/ll_NOBITA_DEFAULTERS_ll")
+            InlineKeyboardButton(text=[𝗖𝗢-𝗗𝗘𝗩], url=f"https://t.me/owner_of_itachi")
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
