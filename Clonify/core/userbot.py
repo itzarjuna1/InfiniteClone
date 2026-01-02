d7f7bb98823f5c967a651cd4f3dcf624559e199a
ARJUN <pachangedinanath@gmail.com>
2026-01-02 16:54:03 +0530

    Update userbot.py

--- Clonify/core/userbot.py
+++ Clonify/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("drxgiveway")
-                await self.one.join_chat("drxgiveway")
+                await self.one.join_chat("snowy_hometown")
+                await self.one.join_chat("snowy_hometown")
             except:
                 pass
             assistants.append(1)
