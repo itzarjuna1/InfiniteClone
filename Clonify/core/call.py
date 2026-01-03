0e1aa59ae52205fd521ce111e0d98b52fa6512d9
ARJUN <pachangedinanath@gmail.com>
2026-01-03 10:48:57 +0530

    Update call.py

--- Clonify/core/call.py
+++ Clonify/core/call.py
@@ -8,12 +8,13 @@ from pyrogram.types import InlineKeyboardMarkup
 
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls.types.stream import (
-    StreamEnded,
-    AudioPiped,
-    AudioVideoPiped,
+from pytgcalls.types.stream import StreamEnded
+from pytgcalls.types.input_streams import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_streams.quality import (
+    HighQualityAudio,
+    MediumQualityVideo,
 )
-from pytgcalls.types.quality import HighQualityAudio, MediumQualityVideo
+
 from ntgcalls import StreamType
 
 import config
