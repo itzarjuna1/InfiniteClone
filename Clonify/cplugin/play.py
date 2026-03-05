b881aa5b590a95d32e0649b7d213282e9ce9edc0
ARJUN <pachangedinanath@gmail.com>
2026-03-05 18:14:57 +0530

    Update play.py

--- Clonify/cplugin/play.py
+++ Clonify/cplugin/play.py
@@ -1260,11 +1260,6 @@ async def stream(
 
 async def get_thumb(videoid):
     try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
+        return config.YOUTUBE_IMG_URL
+    except Exception:
         return config.YOUTUBE_IMG_URL
